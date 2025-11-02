3034b00f59bfcd3667619a8b789ebf3beaa73e37
ArMetka <artem4fight@gmail.com>
2025-11-02 21:30:28 +0700

    m2 t3.1 done

--- module2/3/3.1/chstat.c
+++ module2/3/3.1/chstat.c
@@ -1,4 +1,5 @@
 #include "./chstat.h"
+#include <stdio.h>
 
 int main(int argc, char **argv) {
     char filename[256];
@@ -9,21 +10,15 @@ int main(int argc, char **argv) {
     }
 
     if (filename[0] == '\0' && mode[0] != '\0') { // convert mode to binary (case 1)
-        //
+        return !doConvert(mode);
     } else if (filename[0] != '\0' && mode[0] == '\0') { // stat (case 2)
         return !doStat(filename);
     } else if (filename[0] != '\0' && mode[0] != '\0') { // chmod (case 3)
-        //
+        return !doChmod(filename, mode);
     } else { // shouldn't be here
         printf("Invalid arguments!\n%s\n", USAGE_PROMPT);
         return 1;
     }
-
-    // if (filename[0] != '\0' && stat(filename, &stats) != 0) {
-    //     printf("Failed to get file's stat!\n");
-    //     return 1;
-    // }
-
 
     return 0;
 }
@@ -40,7 +35,7 @@ int readArgs(int argc, char **argv, char *filename, int filename_len, char *mode
         return 0;
     } else {
         for (int i = 1; i < argc; i++) {
-            if (argv[i][0] == '-') {
+            if (argv[i][0] == '-' && argv[i][1] != '-') {
                 switch (argv[i][1]) {
                     case 'f':
                         if (i == argc - 1) {
@@ -63,16 +58,154 @@ int readArgs(int argc, char **argv, char *filename, int filename_len, char *mode
     return 1;
 }
 
+int doConvert(const char *mode) {
+    unsigned int mode_new;
+
+    if (!calculateNewMode(mode, 0, &mode_new)) {
+        return 0;
+    }
+
+    printMode(mode_new);
+
+    return 1;
+}
+
 int doStat(const char *filename) {
+    unsigned int mode = 0;
+
+    if (!getStat(filename, &mode)) {
+        return 0;
+    }
+
+    printMode(mode);
+
+    return 1;
+}
+
+int doChmod(const char *filename, const char *mode) {
+    unsigned int mode_old = 0;
+    unsigned int mode_new = 0;
+
+    if (!getStat(filename, &mode_old)) {
+        return 0;
+    }
+
+    if (!calculateNewMode(mode, mode_old, &mode_new)) {
+        return 0;
+    }
+
+    printf("(old) ");
+    printMode(mode_old);
+    printf("(new) ");
+    printMode(mode_new);
+
+    return 1;
+}
+
+int calculateNewMode(const char *mode, unsigned int mode_old, unsigned int *mode_new) {
+    *mode_new = 0;
+
+    if (mode[0] <= '7' && mode[0] >= '0') {
+        // parse as number (755)
+        unsigned int mode_tmp;
+        if (sscanf(mode, "%u", &mode_tmp) != 1) {
+            printf("Invalid mode format!\n");
+            return 0;
+        }
+        *mode_new = ((mode_tmp / 100) << 6) + ((mode_tmp / 10 % 10) << 3) + mode_tmp % 10;
+    } else if (mode[0] == 'r' || mode[0] == '-') {
+        // parse as string (rwxr-xr-x)
+        for (int i = 0; i < 9; i++) {
+            if (((i % 3 == 0) && mode[i] == 'r') || ((i % 3 == 1) && mode[i] == 'w') || ((i % 3 == 2) && mode[i] == 'x')) {
+                *mode_new = *mode_new | (1 << (8 - i));
+            } else if (mode[i] == '-') {
+                continue; // 0
+            } else {
+                printf("Invalid mode format!\n");
+                return 0;
+            }
+        }
+    } else if (mode[0] == 'u' || mode[0] == 'g' || mode[0] == 'o' || mode[0] == 'a') {
+        // parse as string (u+rx,g+x,o-r)
+        for (int i = 0; mode[i] != '\0'; mode_old = *mode_new) {
+            unsigned int users_mask = 0;
+            unsigned int access_mask = 0;
+            char op = '\0';
+
+            for (; mode[i] != '+' && mode[i] != '-' && mode[i] != '='; i++) {
+                switch (mode[i]) {
+                    case 'u':
+                        users_mask |= U_MASK;
+                        break;
+                    case 'g':
+                        users_mask |= G_MASK;
+                        break;
+                    case 'o':
+                        users_mask |= O_MASK;
+                        break;
+                    case 'a':
+                        users_mask |= A_MASK;
+                        break;
+                    default:
+                        printf("Invalid mode format!\n");
+                        return 0;
+                }
+            }
+
+            op = mode[i++];
+
+            for (; mode[i] != '\0' && mode[i] != ','; i++) {
+                switch (mode[i]) {
+                    case 'r':
+                        access_mask |= R_MASK;
+                        break;
+                    case 'w':
+                        access_mask |= W_MASK;
+                        break;
+                    case 'x':
+                        access_mask |= X_MASK;
+                        break;
+                    default:
+                        printf("Invalid mode format!\n");
+                        return 0;
+                }
+            }
+
+            switch (op) {
+                case '+':
+                    *mode_new = mode_old | (users_mask & access_mask);
+                    break;
+                case '-':
+                    *mode_new = mode_old & ~(users_mask & access_mask);
+                    break;
+                case '=':
+                    *mode_new = (mode_old & ~users_mask) | (users_mask & access_mask);
+                    break;
+                default:
+                    printf("Invalid mode format!\n");
+                    return 0;
+            }
+
+            if (mode[i++] == '\0') {
+                return 1;
+            }
+        }
+    } else {
+        printf("Invalid mode format!\n");
+        return 0;
+    }
+
+    return 1;
+}
+
+int getStat(const char *filename, unsigned int *mode) {
     struct stat stats;
-    unsigned int stat_mode = 0;
 
     if (stat(filename, &stats) != 0) {
         printf("Failed to get file's stat!\n");    
         return 0;
     }
-    stat_mode = stats.st_mode & (S_IRWXU | S_IRWXG | S_IRWXO); // rwx for user, group and others (9 last bits)
-    printMode(stat_mode);
+    *mode = stats.st_mode & (S_IRWXU | S_IRWXG | S_IRWXO); // rwx for user, group and others (9 last bits)
 
     return 1;
 }
--- module2/3/3.1/chstat.h
+++ module2/3/3.1/chstat.h
@@ -7,12 +7,83 @@
 
 #define USAGE_PROMPT "Usage: ./chstat [MODE] [-f FILE]"
 
+#define U_MASK 0b111000000
+#define G_MASK 0b000111000
+#define O_MASK 0b000000111
+#define A_MASK (U_MASK | G_MASK | O_MASK)
+#define R_MASK 0b100100100
+#define W_MASK 0b010010010
+#define X_MASK 0b001001001
+
 int readArgs(int argc, char **argv, char *filename, int filename_len, char *mode, int mode_len);
-// int doConvert();
+int doConvert(const char *mode);
 int doStat(const char *filename);
-// int doChmod();
+int doChmod(const char *filename, const char *mode);
+int calculateNewMode(const char *mode, unsigned int mode_old, unsigned int *mode_new);
+int getStat(const char *filename, unsigned int *mode);
 void printMode(unsigned int mode);
 void modeToString(unsigned int mode, char *string);
 void modeToBinary(unsigned int mode, char *string);
+
+// supported mode formats:
+// - [0-7][0-7][0-7]
+// - ([r-][w-][x-]){3}
+// - [ugoa]+[-+=]([rwx]){0,3}(","[ugoa]+[-+=]([rwx]){0,3})*
+
+// examples:
+// - 755
+// - rwxr-xr-x
+// - u=rwx,g=rx,o=rx
+// - a+x
+
+// if 755:
+//     (1)
+//     new = ((755 / 100) << 6) + ((755 / 10 % 10) << 3) + 755 % 10
+//
+// if u=rx:
+//     (1)
+//     mask of u (111000000)
+//     mask of rx (101101101)
+//     & (101000000)
+//     
+//     (2)
+//     mask of u (111000000)
+//     ~ (000111111)
+//     
+//     (3)
+//     old_stat (.........)
+//     2 result (000111111)
+//     & (000......)
+//
+//     (4)
+//     3 result (000......)
+//     1 result (101000000)
+//     | (101......)
+//     
+// if u+x:
+//     (1)
+//     mask of u (111000000)
+//     mask of x (001001001)
+//     & (001000000)
+//     
+//     (2)
+//     old_stat (.........)
+//     1 result (001000000)
+//     | (..1......)
+//
+// if u-x:
+//     (1)
+//     mask of u (111000000)
+//     mask of x (001001001)
+//     & (001000000)
+//     
+//     (2)
+//     1 result (001000000)
+//     ~ (110111111)
+//     
+//     (3)
+//     old_stat (.........)
+//     2 result (110111111)
+//     & (..0......)
 
 #endif
